ef9a1c3ede11858aa27b6240359c3195a33fdccb
Cyrus440 <62287972+Cyrus440@users.noreply.github.com>
2021-11-04 21:15:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QFileDialog>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QFileDialog dialog(this);
-    dialog.setFileMode(QFileDialog::ExistingFile);
-    dialog.setViewMode(QFileDialog::Detail);
-    dialog.setNameFilter("Muzyka (*.mp3 *)");
-    QStringList fileNames;
-    if (dialog.exec())
-        fileNames = dialog.selectedFiles();
     w.show();
     return a.exec();
 }
