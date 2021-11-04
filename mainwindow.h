77d7187e37c4c3ccd322ddc2a512bf69b6155d9e
Cyrus440 <62287972+Cyrus440@users.noreply.github.com>
2021-11-04 21:18:12 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 #include <QFileDialog>
+#include <QtMultimedia/QAudio>
+#include <QtMultimedia/QMediaPlayer>
+#include <QDebug>
+#include <QtMultimedia/QAudioOutput>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,7 +20,24 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_horizontalSlider_Progress_sliderMoved(int position);
+
+    void on_horizontalSlider_Volume_sliderMoved(int position);
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_positionChanged(qint64 position);
+
+    void on_durationChanged(qint64 position);
+
 private:
     Ui::MainWindow *ui;
+    QMediaPlayer *player;
+    QAudioOutput *audioOutput;
 };
 #endif // MAINWINDOW_H
